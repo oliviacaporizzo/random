c185e680c8e8097e12e7099960d37e75fd59acf8
Olivia Caporizzo <ocapori1@jhu.edu>
2016-06-27 11:26:55 -0400

    working basic mailform with style

--- mailTo.php
+++ mailTo.php
@@ -3,10 +3,12 @@
 	$visitor_email = $_POST['email'];
 	$message = $_POST['message'];
 
-	$to = "o.caporizzo@gmail.com";
+	$to = 'o.caporizzo@gmail.com';
 	$headers = "From: $visitor_email \r\n";
-	$subject = "Form Submission from Take a Byte"
-	$body = $message;
-	
-	mail($to, $subject, $body, $headers);
+	$subject = "Form Submission from Take a Byte";
+	$body = "From: $name \n Message: $message";
+
+	$result = mail($to, $subject, $body, $headers) or die("ERROR");
+	echo $result;
+
 ?>
